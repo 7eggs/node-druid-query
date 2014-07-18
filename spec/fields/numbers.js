74f6e06977f51bb9581f10b7d2c72fe918037a93
Alexander Makarenko <estliberitas@gmail.com>
2014-07-18 15:40:20 +0400

    test(numbers): add tests for number fields

---
+++ spec/fields/numbers.js
@@ -0,0 +1,42 @@
+'use strict'
+
+var expect = require('expect.js')
+  , Query = require('./query')
+
+
+describe('Query (numbers)', function() {
+  var query
+
+  beforeEach(function() {
+    query = new Query()
+  })
+
+  describe('#threshold()', function() {
+    it('should set value', function() {
+      var raw = query.threshold(1).toJSON()
+
+      expect(raw.threshold).to.be(1)
+      expect(raw.threshold).to.be.a('number')
+    })
+
+    it('should parse string and set value', function() {
+      var raw = query.threshold('1').toJSON()
+
+      expect(raw.threshold).to.be(1)
+      expect(raw.threshold).to.be.a('number')
+    })
+
+    it('should parse float and set value', function() {
+      var raw = query.threshold('1.5').toJSON()
+
+      expect(raw.threshold).to.be(1.5)
+      expect(raw.threshold).to.be.a('number')
+    })
+
+    it('should throw error when can\'t parse number', function() {
+      expect(function() {
+        query.threshold('NaN').toJSON()
+      }).to.throwException()
+    })
+  })
+})
