ba7ed1c4794ac21a47500e03b512cad50aff0b13
Alexander Makarenko <estliberitas@gmail.com>
2014-07-18 15:38:57 +0400

    test(strings): add more tests for string fields

---
+++ spec/fields/strings.js
@@ -0,0 +1,45 @@
+'use strict'
+
+var expect = require('expect.js')
+  , Query = require('./query')
+
+
+describe('Query (strings)', function() {
+  var query
+
+  beforeEach(function() {
+    query = new Query()
+  })
+
+  describe('#dataSource()', function() {
+    it('should set value', function() {
+      var raw = query.dataSource('sample_datasource').toJSON()
+
+      expect(raw.dataSource).to.be('sample_datasource')
+      expect(raw.dataSource).to.be.a('string')
+    })
+
+    it('should convert any input value to string', function() {
+      var raw = query.dataSource(new String('sample_datasource')).toJSON()
+
+      expect(raw.dataSource).to.be('sample_datasource')
+      expect(raw.dataSource).to.be.a('string')
+    })
+  })
+
+  describe('#queryType()', function() {
+    it('should set value', function() {
+      var raw = query.queryType('groupBy').toJSON()
+
+      expect(raw.queryType).to.be('groupBy')
+      expect(raw.queryType).to.be.a('string')
+    })
+
+    it('should convert any input value to string', function() {
+      var raw = query.queryType(new String('groupBy')).toJSON()
+
+      expect(raw.queryType).to.be('groupBy')
+      expect(raw.queryType).to.be.a('string')
+    })
+  })
+})
