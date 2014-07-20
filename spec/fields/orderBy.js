8ee4522175a720aa11ed14e6f5ac939239d388dc
Alexander Makarenko <estliberitas@gmail.com>
2014-07-21 03:42:35 +0400

    test(Query/orderBy): add test suite

---
+++ spec/fields/orderBy.js
@@ -0,0 +1,35 @@
+'use strict'
+
+var expect = require('expect.js')
+  , Query = require('./query')
+
+
+describe('Query', function() {
+  var query
+
+  describe('.orderBy()', function() {
+    it('should create spec', function() {
+      var spec = Query.orderBy('dimension1', 'descending')
+
+      expect(spec).to.eql({dimension: 'dimension1', direction: 'descending'})
+    })
+
+    it('should create spec (string)', function() {
+      var spec = Query.orderBy('dimension2')
+
+      expect(spec).to.eql({dimension: 'dimension2', direction: 'ASCENDING'})
+    })
+
+    it('should throw error if dimension is not specified', function() {
+      expect(function() {
+        query.orderBy(null, 'foobar')
+      }).to.throwException()
+    })
+
+    it('should throw error if direction is invalid', function() {
+      expect(function() {
+        query.orderBy('dimension3', 'foobar')
+      }).to.throwException()
+    })
+  })
+})
