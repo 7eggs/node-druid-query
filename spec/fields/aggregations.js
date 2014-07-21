c31a391225d7bdf3cb5b887fa79bd5b59e98fe29
Alexander Makarenko <estliberitas@gmail.com>
2014-07-21 15:40:53 +0400

    refactor(tests): separate aggregation tests in two parts

--- spec/fields/aggregations.js
+++ spec/fields/aggregations.js
@@ -7,64 +7,6 @@ var noop = function() {
 }
 
 
-describe('Query', function() {
-  describe('.aggregation()', function() {
-    it('should create spec', function() {
-      var spec = Query.aggregation('count', 'output')
-
-      expect(spec.type).to.be('count')
-      expect(spec.name).to.be('output')
-    })
-
-    it('should create spec from raw spec', function() {
-      var spec = Query.aggregation({type: 'count', name: 'output'})
-
-      expect(spec.type).to.be('count')
-      expect(spec.name).to.be('output')
-    })
-
-    it('should throw error for missing type', function() {
-      expect(function() {
-        var spec = Query.aggregation('missing', 'aggregation')
-      }).to.throwException()
-    })
-
-    it('should throw error for missing name', function() {
-      expect(function() {
-        var spec = Query.aggregation('count')
-      }).to.throwException()
-    })
-  })
-
-  describe('#aggregation()', function() {
-    var query
-
-    beforeEach(function() {
-      query = new Query()
-    })
-
-    it('should add spec to aggregations', function() {
-      query.aggregation('count', 'output')
-      var raw = query.toJSON()
-
-      expect(raw.aggregations).to.have.length(1)
-      expect(raw.aggregations[0].type).to.be('count')
-      expect(raw.aggregations[0].name).to.be('output')
-    })
-
-    it('should add raw spec to aggregations', function() {
-      query.aggregation({type: 'count', name: 'output'})
-      var raw = query.toJSON()
-
-      expect(raw.aggregations).to.be.an(Array)
-      expect(raw.aggregations).to.have.length(1)
-      expect(raw.aggregations[0].type).to.be('count')
-      expect(raw.aggregations[0].name).to.be('output')
-    })
-  })
-})
-
-
 describe('Aggregations', function() {
   describe('count', function() {
     it('should create spec', function() {
