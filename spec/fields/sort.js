e036eba80d344ded296d3cd74adfae5c1094b5a2
Alexander Makarenko <estliberitas@gmail.com>
2014-07-21 18:11:06 +0400

    test(Query/sort): add test suite

---
+++ spec/fields/sort.js
@@ -0,0 +1,35 @@
+'use strict'
+
+var expect = require('expect.js')
+  , Query = require('./query')
+
+
+describe('Query', function() {
+  var query
+
+  beforeEach(function() {
+    query = new Query()
+  })
+
+  describe('#sort()', function() {
+    it('should set sort spec', function() {
+      var raw = query.sort('lexicographic').toJSON()
+
+      expect(raw.sort).to.eql({
+        type:  'lexicographic'
+      })
+    })
+
+    it('should throw error if bad type is specified', function() {
+      expect(function() {
+        query.sort()
+      }).to.throwException()
+    })
+
+    it('should throw error if bad type is specified', function() {
+      expect(function() {
+        query.sort('bad_type')
+      }).to.throwException()
+    })
+  })
+})
